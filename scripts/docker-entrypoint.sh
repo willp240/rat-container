0dafe2866006072f29e83359be61b244e1df0184
Mark Anderson <mark.russ.anderson@gmail.com>
2021-10-18 15:17:35 -0400

    Remove most code and use setup_env script.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-SL_PYTHON27=/opt/rh/python27/enable
-if [ -f "$SL_PYTHON27" ]; then
-   source $SL_PYTHON27
-fi
-
-source /home/software/root/bin/thisroot.sh
-source /home/software/geant4.10.00.p04/bin/geant4.sh
-
-# export RAT_SCONS=/home/software/scons-3.1.2
-
-# export TF_DIR=/usr/local
-# export CPPFLOW_DIR=/home/software/cppflow
-# export LIBRARY_PATH=$LIBRARY_PATH:$TF_DIR/lib
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TF_DIR/lib
-# export LIBRARY_PATH=$LIBRARY_PATH:$CPPFLOW_DIR/lib
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPPFLOW_DIR/lib
-
-RAT_ENV=/rat/env.sh
-if [ -f "$RAT_ENV" ]; then
-    source $RAT_ENV
-else
-    printf "\nCould not find /rat/env.sh\nIf youre building RAT, please ignore.\nOtherwise, ensure RAT is mounted to /rat\n"
-fi
+source /home/scripts/setup-env.sh
 
 /bin/bash "$@"
